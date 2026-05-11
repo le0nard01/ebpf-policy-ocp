3fb96d2b19e5ffbd84d21c701495137c47f65b0f
Leonardo Oste <68928875+le0nard01@users.noreply.github.com>
2026-05-11 15:20:15 -0300

    Initial commit

---
+++ policies/code/block_af_alg.bpf.c
@@ -0,0 +1,18 @@
+// Blocks creation of AF_ALG sockets by overriding __sys_socket return value.
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define AF_ALG 38
+#define EPERM 1
+
+SEC("kprobe/__sys_socket")
+int BPF_KPROBE(block_af_alg_socket, int family, int type, int protocol)
+{
+    if (family == AF_ALG) {
+        bpf_override_return(ctx, -EPERM);
+    }
+
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
